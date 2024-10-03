bc085156a4905ac8f1eb47b66ccac32787dcd0df
SHIN SUJUNG <shinssjj79@gmail.com>
2024-10-03 22:47:58 +0900

    Initial commit

---
+++ src/main/java/com/example/notepadP/NotepadPApplication.java
@@ -0,0 +1,13 @@
+package com.example.notepadP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotepadPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotepadPApplication.class, args);
+	}
+
+}
